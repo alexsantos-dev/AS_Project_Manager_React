6f46e2bd0e9e61cf890de0d53daa921c28c479d7
Alex Santos <alexsantos.web.developer@gmail.com>
2023-11-14 19:37:06 -0300

    alterações de estilo

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -6,9 +6,9 @@ function Footer(){
         <footer className={styles.footer}>
             <ul className={styles.social_list}>
             <p className={styles.copy_right}><span>Alex Santos</span> &copy; 2023</p>
-                <li><a href="https://www.linkedin.com/in/alex-santos-b020b5296"><FaLinkedin/></a></li>
-                <li><a href="https://github.com/alexsantos-dev"><FaGithub/></a></li>
-                <li><a href="https://wa.me/+5598981630767"><FaWhatsapp/></a></li>
+                <li><a target="_blank" rel="noreferrer"  href="https://www.linkedin.com/in/alex-santos-b020b5296"><FaLinkedin/></a></li>
+                <li><a target="_blank" rel="noreferrer" href="https://github.com/alexsantos-dev"><FaGithub/></a></li>
+                <li><a target="_blank" rel="noreferrer"  href="https://wa.me/+5598981630767"><FaWhatsapp/></a></li>
             </ul>
         </footer>
     )
